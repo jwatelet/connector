3441702dd54984402efb4687053d192d519b3930
mentegy <mentegy@gmail.com>
2015-07-27 16:58:37 +0300

    rework configs (all constants are lazy val now )

--- src/main/scala/connector/core/Boot.scala
+++ src/main/scala/connector/core/Boot.scala
@@ -13,40 +13,40 @@ import scala.concurrent.duration._
 
 object Boot extends App {
 
-  import Configuration._
-
-  implicit val system = ActorSystem(config.getString("app.actor-system.name"))
+  implicit val system = ActorSystem(Config.actorSystemName)
   implicit val timeout = Timeout(15.seconds)
 
   val application = system.actorOf(Props[ApplicationActor], "connector-service")
-  val tweeter = system.actorOf(Props(classOf[TweeterStreamingActor], sparkConf, optionsForRDBMS), "spark-app")
+  val tweeter = system.actorOf(Props(classOf[StreamingActor]), "spark-app")
 
   IO(Http) ? Http.Bind (
     listener = application,
-    interface = config.getString("app.server.host"),
-    port = config.getInt("app.server.port")
+    interface = Config.interface,
+    port = Config.port
   )
-
 }
 
-object Configuration {
+object Config {
 
-  val config = ConfigFactory.load()
-  val twitterConf = ConfigFactory.load("twitter")
+  private val config = ConfigFactory.load()
+  private val twitterConf = ConfigFactory.load("twitter")
 
   System.setProperty("twitter4j.oauth.consumerKey", twitterConf.getString("consumerKey"))
   System.setProperty("twitter4j.oauth.consumerSecret", twitterConf.getString("consumerSecret"))
   System.setProperty("twitter4j.oauth.accessToken", twitterConf.getString("accessToken"))
   System.setProperty("twitter4j.oauth.accessTokenSecret", twitterConf.getString("accessTokenSecret"))
 
-  val optionsForRDBMS = (query: String) => Map (
+  lazy val interface = config.getString("app.server.host")
+  lazy val port = config.getInt("app.server.port")
+  lazy val actorSystemName = config.getString("app.actor-system.name")
+
+  lazy val optionsForRDBMS = Map (
     "driver" -> config.getString("app.database.driver"),
     "url" -> config.getString("app.database.url"),
-    "dbtable" -> query // can be either table name or select query
+    "dbtable" -> config.getString("app.database.query")
   )
 
-  val sparkConf = new SparkConf()
+  lazy val sparkConf = new SparkConf()
     .setMaster(config.getString("app.spark.master-uri"))
     .setAppName(config.getString("app.spark.app-name"))
-
 }
